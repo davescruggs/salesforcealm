c3413502ab8b40dbf8e017c1b32e9165add3c02e
David Scruggs <dscruggs@dscruggs-ltm.internal.salesforce.com>
2018-02-05 16:51:20 -0500

    updated .travis.yml

---
+++ setenv.sh
@@ -0,0 +1,4 @@
+#!/bin/zsh
+export CONSUMER_KEY=3MVG9mclR62wycM0jYGSqgleiCxolS5U.3FN8e8wlP7YHT5CbjezbIZsk2b0cpmtUNXLWEWreBQeFU44VXDCB
+export JWT_KEY_FILE=/Users/dscruggs/SFDemo/SFDX/salesforcealm/assets/server.key
+export HUB_USERNAME=dscruggs@sdo2018.mds
